941422ce11a0f112efbf3c4b51b3043b6b995132
Moritz <moritz.vogel3@gmail.com>
2017-06-23 18:22:38 +0200

    Added getServer method to service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,6 +7,10 @@ import {
   Headers
 } from '@angular/http';
 
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 /**
  * This service is used to easy communication with the CentauriApi module.
  * This service offers two kinds of methods, all returning an object of 
@@ -24,6 +28,30 @@ import {
 @Injectable()
 export class ApiService {
 
+  private static INTERVAL = 3000;
+
   constructor(private http: Http) { }
+
+  /**
+   * Get the currently running servers on the network
+   * @return {Observable}
+   */
+  getServers(): Observable<any>{
+    // TODO implement api logic
+    return null;
+  }
+
+  /**
+   * Get the subscriber wrapper for currently active servers
+   * @param {Number} interval Refresh pull interval 
+   * @returns {Observable} 
+   */
+  getServersSubscriber(interval = ApiService.INTERVAL): Observable<any> {
+    return Observable
+        .interval(interval)
+        .flatMap(() => {
+          return this.getServers();
+        });
+  }
 
 }
